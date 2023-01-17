e217da70efdd9050af6750caba519461624508fc
elfarsi rajae <elfarsirajae@gmail.com>
2023-01-18 00:05:34 +0100

    commande_produit_premier_push

--- src/main/java/com/myresto/dao/ProduitDao.java
+++ src/main/java/com/myresto/dao/ProduitDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.myresto.domaine.Commande;
 import com.myresto.domaine.Produit;
 
 @Repository
@@ -15,7 +16,15 @@ public class ProduitDao implements IProduitDao{
 	public ProduitDao(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
-	
+	//READ
+		@Override
+		public Produit getProduitById(int id ) {
+			Object[] arguments = new Object[1];
+			arguments[0]= id;
+			return (Produit) jdbcTemplate.query("Select * from from myresto.produit where id =?", (resultSet, RowNum) ->{
+				return new Produit(resultSet.getInt("id"),resultSet.getInt("id"),resultSet.getString("libelle"),resultSet.getDouble("prix"));
+			});
+		}
 	//Read
 	public List<Produit> getAllProducts(){
 		return jdbcTemplate.query("Select * from myresto.produit",(resultSet, rowNum) -> {
