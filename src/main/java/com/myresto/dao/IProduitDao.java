fd1552ea0d89615b4c5177b73c592e7a37245469
Aldam <alexis.damotte@outlook.com>
2023-01-18 12:13:09 +0100

    a

--- src/main/java/com/myresto/dao/IProduitDao.java
+++ src/main/java/com/myresto/dao/IProduitDao.java
@@ -2,9 +2,12 @@ package com.myresto.dao;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.myresto.domaine.Commande;
 import com.myresto.domaine.Produit;
 
+@Service
 public interface IProduitDao {
 	List<Produit> getAllProducts();
 	void createProduit(Produit p);
